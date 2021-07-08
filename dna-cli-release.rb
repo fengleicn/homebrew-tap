cf13a56e18d641dbe8a74a3b0c1e0519f5d17d65
冯磊 <673319230@qq.com>
2021-07-08 18:52:19 +0800

    Create dna-cli-release.rb

---
+++ dna-cli-release.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class DnaCliRelease < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/fengleicn/dna-cli-release/releases/download/0.0.1/dna-cli"
+  sha256 "1cdb03a76ff0fe38a1008d200b334d040235ff39994e4e990aac346ea82f51f9"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    bin.install "dna-cli"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test dna-cli-release`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
