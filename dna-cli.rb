3d78c4bb9115820c8ba3a82b35a69660e8f22bb0
冯磊 <673319230@qq.com>
2021-07-09 15:25:15 +0800

    Update dna-cli.rb

--- dna-cli.rb
+++ dna-cli.rb
@@ -1,11 +1,11 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class DnaCli < Formula
-  desc "You can download by yourself from https://github.com/fengleicn/homebrew-tap/releases/download/dna-cli-0.0.1/dna-cli"
+class HomebrewTap < Formula
+  desc ""
   homepage ""
   url "https://github.com/fengleicn/homebrew-tap/releases/download/dna-cli-0.0.1/dna-cli"
-  sha256 "74d5a8ff67a9a7afe0634802cdb6f8215404e4ea2a4bf2d2127a72c55bf63fa5"
+  sha256 "e70c76706ea82755689a7f29f737fd6d28dd9874a179a02875a4065291a96f2d"
   license "BSD-2-Clause"
 
   # depends_on "cmake" => :build
@@ -14,7 +14,7 @@ class DnaCli < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    bin.install "dna-cli"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
@@ -23,7 +23,7 @@ class DnaCli < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test dna-cli`. Options passed
+    # software. Run the test with `brew test homebrew-tap`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
